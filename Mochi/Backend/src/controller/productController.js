8201f36fd00534f34cd16f9c4201a5ec89cf79e1
Vincent <Vincent.rey865@icloud.com>
2024-06-20 18:10:12 +0200

    product controller

--- Mochi/Backend/src/controller/productController.js
+++ Mochi/Backend/src/controller/productController.js
@@ -22,3 +22,47 @@ exports.getProductById = async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+exports.createProduct = async (req, res) => {
+  try {
+    const { name, description, price, img } = req.body;
+    if (!name || !description || !price || !img) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+    const newProduct = await Product.create(req.body);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ error: "Invalid product data" });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  const productId = req.params.id;
+  const update = req.body;
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(productId, update, {
+      new: true,
+    });
+    if (updatedProduct) {
+      res.json({ message: "Product Updated Succsessfully", updatedProduct });
+    } else {
+      res.status(404).json({ error: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  const productId = req.params.id;
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+    if (deletedProduct) {
+      res.json({ message: "product deleted successfully", deletedProduct });
+    } else {
+      res.status(500).json({ error: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
